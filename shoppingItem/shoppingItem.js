faf942e2e800e685d5d4902cfa41c9bfceefdb07
Echo493827 <165719536+Echo493827@users.noreply.github.com>
2025-07-31 00:03:29 +0000

    added shoppingItem react code

---
+++ shoppingItem/shoppingItem.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export default function ShoppingItem(props) {
+  const [quantity, setQuantity] = useState(0);
+
+  function handleIncrease() {
+    setQuantity(quantity + 1);
+  }
+
+  function handleDecrease() {
+    if (quantity > 0) {
+      setQuantity(quantity - 1);
+    }
+  }
+  
+  return (
+    <div className="shopping-item">
+      <p className="item-info">
+        <strong>{props.name}</strong>
+        <div className="buttons">
+          <button className="btn-decrement" onClick={handleDecrease}>-</button>
+          <span className="quantity">{quantity}</span>
+          <button className="btn-increment" onClick={handleIncrease}>+</button>
+        </div>
+      </p>
+    </div>
+  );
+}
